c00eecfe66a68073bec7b6b4c49207f86bd73910
Abdvijay <abdvijay19@gmail.com>
2025-06-26 12:41:12 +0530

    Comments added

--- Python/OOPS/TypesOfMethod.py
+++ Python/OOPS/TypesOfMethod.py
@@ -5,14 +5,20 @@ class Student:
         self.m2 = m2
         self.m3 = m3
 
+    # Class method
+    # Class methods can access class variables and can be called on the class itself.
     @classmethod
     def getCollege(cls):
         return cls.College
     
+    # Static method
+    # Static methods do not access class or instance variables. They are utility functions that belong to the class.
     @staticmethod
     def info():
         print("This is type of method example")
 
+    # Instance method
+    # Instance methods can access instance variables and class variables.
     def avg(self):
         return (self.m1 + self.m2 + self.m3)/3
 
@@ -20,9 +26,9 @@ s1 = Student(50,40,60)
 s2 = Student(10,20,30)
 
 s1.info()
-print(Student.getCollege())
+print(s1.getCollege())
 print(s1.avg())
 
 s2.info()
-print(Student.getCollege())
+print(s2.getCollege())
 print(s2.avg())
