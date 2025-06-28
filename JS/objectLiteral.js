1030dafb0a71bf013db31d3ce4a246b7c2d15d3f
Abdvijay <abdvijay19@gmail.com>
2025-06-28 19:06:48 +0530

    object add,update,delete changes

--- JS/objectLiteral.js
+++ JS/objectLiteral.js
@@ -1,12 +1,18 @@
 let alien = {
-    name : 'Vijay',
-    tech : 'JS',
-    'home loc' : 'Banglore'
-}
-console.log(alien) //{ name: 'Vijay', tech: 'JS', 'home loc': 'Banglore' }
-console.log(alien.name) // Vijay
-console.log(alien['home loc']) // Banglore
+  name: "Vijay",
+  tech: "JS",
+  "home loc": "Banglore",
+};
+console.log(alien); //{ name: 'Vijay', tech: 'JS', 'home loc': 'Banglore' }
+console.log(alien.name); // Vijay
+console.log(alien["home loc"]); // Banglore
 
-alien['age'] = 45;
-alien.country = "India"
+alien["age"] = 45;
+alien.country = "India";
+console.log(alien);
+
+alien.age = 24;
+console.log(alien);
+
+delete alien["home loc"];
 console.log(alien);
