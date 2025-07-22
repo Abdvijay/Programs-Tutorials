aa9802549ad238292cd9befad529e40ee184b7b0
Abdvijay <abdvijay19@gmail.com>
2025-07-22 13:34:45 +0530

    decorator and special variable __name__

---
+++ Python/Modules/CalcProgram.py
@@ -0,0 +1,12 @@
+def add():
+    print("Adding numbers...",__name__)
+
+def sub():
+    print("Subtracting numbers...",__name__)
+
+def main():
+    add()
+    sub()
+
+if __name__ == "__main__":
+    main()
---
+++ Python/Modules/Demo.py
@@ -0,0 +1,10 @@
+from CalcProgram import *
+def funct1():
+    add()
+    print("This is demo program",__name__)
+
+def main():
+    funct1()
+
+if __name__ == "__main__":
+    main()
