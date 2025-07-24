67a0f4b671c1d8c146c060d7b0977180ba1df5b5
Abdvijay <abdvijay19@gmail.com>
2025-07-24 12:58:44 +0530

    Iterator concept added

--- Python/TypesOfArguments.py
+++ Python/TypesOfArguments.py
@@ -13,3 +13,13 @@ def greet(*data):
     for i in data:
         print(i)
 greet("Vijay", "Swathi", "Essai") #Variable Length Arguments
+
+class Student:
+    def sum(self,*data):
+        total = 0
+        for i in data:
+            total += i
+        print("Sum is : ", total)
+
+s1 = Student()
+s1.sum(14,56,78,90) #Variable Length Arguments in Class Method
