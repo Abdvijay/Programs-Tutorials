1557289af1be2e78101f336a0b827edc665e459b
Abdvijay <abdvijay19@gmail.com>
2025-08-06 14:08:53 +0530

    String Functions

---
+++ Python/StringFunctions.py
@@ -0,0 +1,37 @@
+my_string = "hi Vijay how are you ?"
+
+#Printing
+print(my_string)
+
+#Uppercase
+print(my_string.upper())
+
+#Lowercase
+print(my_string.lower())
+
+#Capitalize
+print(my_string.capitalize())
+
+#Length
+print(len(my_string))
+
+#Extract
+print(my_string[3:8])
+
+#Extract First Letter
+print(my_string[0])
+
+#Extract Last Letter
+print(my_string[-1])
+
+#Replace
+print(my_string.replace('Vijay','Swathi'))
+
+#Count
+print(my_string.count('Vijay'))
+
+#Find the Index of Substring
+print(my_string.find('Vijay'))
+
+#Spliting
+print(my_string.split())
