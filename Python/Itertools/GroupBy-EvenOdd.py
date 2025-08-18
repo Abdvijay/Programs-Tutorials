b1b2b3cdff946063da2a699817739be5d9a9834e
Abdvijay <abdvijay19@gmail.com>
2025-08-18 13:56:47 +0530

    GroupBy-Even output added

--- Python/Itertools/GroupBy-EvenOdd.py
+++ Python/Itertools/GroupBy-EvenOdd.py
@@ -6,3 +6,10 @@ g_data = groupby(items, key = lambda x: 'Even' if x % 2 == 0 else 'Odd')
 
 for key, group in g_data:
     print(f'Group : {key} -> {list(group)}')
+
+# Group : Even -> [2, 4, 6]
+# Group : Odd -> [7]
+# Group : Even -> [8, 10]
+# Group : Odd -> [11]
+# Group : Even -> [12]
+# Group : Odd -> [13]
